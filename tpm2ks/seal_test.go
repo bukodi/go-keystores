b996f6e9dd8d8b547dbf1f344cbae65d869623d9
Bukodi László <laszlo.bukodi@noreg.hu>
2023-03-01 12:26:38 +0100

    tpm test: eliminate import for srktemplateECC

--- tpm2ks/seal_test.go
+++ tpm2ks/seal_test.go
@@ -6,7 +6,6 @@ package tpm2ks
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/google/go-tpm-tools/client"
 	"github.com/google/go-tpm/tpm2"
 	"github.com/google/go-tpm/tpmutil"
 	"io"
@@ -14,23 +13,21 @@ import (
 )
 
 var (
-	// Default EK template defined in:
-	// https://trustedcomputinggroup.org/wp-content/uploads/Credential_Profile_EK_V2.0_R14_published.pdf
-	// Shared SRK template based off of EK template and specified in:
-	// https://trustedcomputinggroup.org/wp-content/uploads/TCG-TPM-v2.0-Provisioning-Guidance-Published-v1r1.pdf
 	srkTemplate = tpm2.Public{
-		Type:       tpm2.AlgRSA,
+		Type:       tpm2.AlgECC,
 		NameAlg:    tpm2.AlgSHA256,
-		Attributes: tpm2.FlagFixedTPM | tpm2.FlagFixedParent | tpm2.FlagSensitiveDataOrigin | tpm2.FlagUserWithAuth | tpm2.FlagRestricted | tpm2.FlagDecrypt | tpm2.FlagNoDA,
-		AuthPolicy: nil,
-		RSAParameters: &tpm2.RSAParams{
+		Attributes: tpm2.FlagStorageDefault | tpm2.FlagNoDA,
+		ECCParameters: &tpm2.ECCParams{
 			Symmetric: &tpm2.SymScheme{
 				Alg:     tpm2.AlgAES,
 				KeyBits: 128,
 				Mode:    tpm2.AlgCFB,
 			},
-			KeyBits:    2048,
-			ModulusRaw: make([]byte, 256),
+			CurveID: tpm2.CurveNISTP256,
+			Point: tpm2.ECPoint{
+				XRaw: make([]byte, 32),
+				YRaw: make([]byte, 32),
+			},
 		},
 	}
 )
@@ -47,24 +44,15 @@ func run() (retErr error) {
 	if err != nil {
 		return fmt.Errorf("can't open TPM %q: %v", tpmPath, err)
 	}
-	defer func() {
-		if err := rwc.Close(); err != nil {
-			retErr = fmt.Errorf("%v\ncan't close TPM %q: %v", retErr, tpmPath, err)
-		}
-	}()
+	defer rwc.Close()
 
 	// Create the parent key against which to seal the data
 	srkPassword := ""
-	srkTemplate = client.SRKTemplateECC()
 	srkHandle, _, err := tpm2.CreatePrimary(rwc, tpm2.HandleOwner, tpm2.PCRSelection{}, "", srkPassword, srkTemplate)
 	if err != nil {
 		return fmt.Errorf("can't create primary key: %v", err)
 	}
-	defer func() {
-		if err := tpm2.FlushContext(rwc, srkHandle); err != nil {
-			retErr = fmt.Errorf("%v\nunable to flush SRK handle %q: %v", retErr, srkHandle, err)
-		}
-	}()
+	defer tpm2.FlushContext(rwc, srkHandle)
 	fmt.Printf("Created parent key with handle: 0x%x\n", srkHandle)
 
 	// Get the authorization policy that will protect the data to be sealed
@@ -85,8 +73,7 @@ func run() (retErr error) {
 	if err != nil {
 		return fmt.Errorf("unable to seal data: %v", err)
 	}
-	fmt.Printf("Sealed data: 0x%x\n", privateArea)
-	fmt.Printf("Sealed data public and private:\n%s\n\n%s\n", hex.EncodeToString(publicArea), hex.EncodeToString(privateArea))
+	fmt.Printf("Sealed data\n - public : %s\n - private: %s\n\n", hex.EncodeToString(publicArea), hex.EncodeToString(privateArea))
 
 	//publicArea, _ = hex.DecodeString("0008000b0000001200208fcd2169ab92694e0c633f1ab772842b8241bbc20288981fc7ac1eddc1fddb0e001000202b3046c35e30c79b9d09d672d64d664b3ee9724741829a756bb8f6e6582af1ab")
 	//privateArea, _ = hex.DecodeString("00208e7732c843d7cb45288164f7ad8713eeb7b39398ddd752fbd16d2addb140e80700104003c6232480d87fa1d63b0678cb60d770425331f63d08ca2554603cc30c3961a8262bcd8cf2941f2751c77dacbf2ea079e62aaf22c60ddf705fe719b4c61ae822e460847a4cfe312ab1671e0dc8fe5bb2ec38ba37189e03abcc4a74fefe5008")
